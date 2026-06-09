d0fb9422c0a97f0db625250d62b310a58666756e
mejiasmaximiliano-cmd <mejiasmaximiliano@gmail.com>
2026-06-09 11:23:31 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,4 @@
-// public/sw.js - El motor que hace la app instalable
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] RosterMax Instalado');
+  console.log('[Service Worker] Instalado');
 });
-
-// Este evento fetch vacío es el requisito técnico obligatorio de Google y Apple 
-// para que el celular active el botón de "Instalar App".
 self.addEventListener('fetch', (e) => {});
